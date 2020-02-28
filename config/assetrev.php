3e34a5b46dbe92cf462207a355908e86cc762c89
poopsplat <matt.m.soria@gmail.com>
2020-02-28 14:33:07 -0600

    Fixing assetrev settings

--- config/assetrev.php
+++ config/assetrev.php
@@ -1,5 +1,20 @@
 <?php
-return [
-  'manifestPath' => 'web/assets/dist/rev-manifest.json',
-  'assetUrlPrefix' => '@web/assets/dist/',
-];
+return array(
+    '*' => array(
+        'strategies' => [
+            'manifest' => \club\assetrev\utilities\strategies\ManifestFileStrategy::class,
+            'querystring' => \club\assetrev\utilities\strategies\QueryStringStrategy::class,
+            'passthrough' => function ($filename, $config) {
+                return $filename;
+            },
+        ],
+        'pipeline' => 'manifest|querystring|passthrough',
+        'manifestPath' => 'web/assets/dist/mix-manifest.json',
+        'assetsBasePath' => 'web/assets/dist/',
+        'assetUrlPrefix' => '@web/assets/dist/',
+    ),
+    'dev' => array(
+      'pipeline' => 'passthrough',
+      'manifestPath' => null,
+    )
+);
